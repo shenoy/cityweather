d2858042f0aabd59b3f41acfad0f85c83d44c5fa
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-04-26 23:01:47 +0100

    added cities and temperature in degrees

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -23,15 +23,42 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="ui input">
-        <input
-          onChange={this.onInputChange}
-          type="text"
-          placeholder="Search..."
-          value={this.state.term}
-        />
-        <button className="ui button ">Search</button>
-      </form>
+      <div>
+        <h2>Weather forecast for next 3 days in the USA</h2>
+        <h4>Search from below or any city</h4>
+        <p>
+          {" "}
+          Alabama (AL) - Montgomery Alaska (AK) - Juneau Arizona (AZ) - Phoenix
+          Arkansas (AR) - Little Rock California (CA) - Sacramento Colorado (CO)
+          - Denver Connecticut (CT) - Hartford Delaware (DE) - Dover Florida
+          (FL) - Tallahassee Georgia (GA) - Atlanta Hawaii (HI) - Honolulu Idaho
+          (ID) - Boise Illinois (IL) - Springfield Indiana (IN) - Indianapolis
+          Iowa (IA) - Des Moines Kansas (KS) - Topeka Kentucky (KY) - Frankfort
+          Louisiana (LA) - Baton Rouge Maine (ME) - Augusta Maryland (MD) -
+          Annapolis Massachusetts (MA) - Boston Michigan (MI) - Lansing
+          Minnesota (MN) - St. Paul Mississippi (MS) - Jackson Missouri (MO) -
+          Jefferson City Montana (MT) - Helena Nebraska (NE) - Lincoln Nevada
+          (NV) - Carson City New Hampshire (NH) - Concord New Jersey (NJ) -
+          Trenton New Mexico (NM) - Santa Fe New York (NY) - Albany North
+          Carolina (NC) - Raleigh North Dakota (ND) - Bismarck Ohio (OH) -
+          Columbus Oklahoma (OK) - Oklahoma City Oregon (OR) - Salem
+          Pennsylvania (PA) - Harrisburg Rhode Island (RI) - Providence South
+          Carolina (SC) - Columbia South Dakota (SD) - Pierre Tennessee (TN) -
+          Nashville Texas (TX) - Austin Utah (UT) - Salt Lake City Vermont (VT)
+          - Montpelier Virginia (VA) - Richmond Washington (WA) - Olympia West
+          Virginia (WV) - Charleston Wisconsin (WI) - Madison Wyoming (WY) -
+          Cheyenne{" "}
+        </p>
+        <form onSubmit={this.onFormSubmit} className="ui input">
+          <input
+            onChange={this.onInputChange}
+            type="text"
+            placeholder="Search..."
+            value={this.state.term}
+          />
+          <button className="ui button ">Search</button>
+        </form>
+      </div>
     );
   }
 
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Chart from '../components/chart';
-import Googlemap from '../components/Googlemap'
-
+import React from "react";
+import { connect } from "react-redux";
+import Chart from "../components/chart";
+import Googlemap from "../components/Googlemap";
 
 class WeatherList extends React.Component {
-
-
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map((x) => x.main.temp);
-    const pressures = cityData.list.map((x) => x.main.pressure);
-    const humidities = cityData.list.map((x) => x.main.humidity);
+    const temps = cityData.list.map(x => x.main.temp - 273);
+    const pressures = cityData.list.map(x => x.main.pressure);
+    const humidities = cityData.list.map(x => x.main.humidity);
     const { lon, lat } = cityData.city.coord;
-
 
     return (
       <tr key={name}>
-        <td><Googlemap lon={lon} lat={lat} /></td>
-        <td><Chart data={temps} color="red" units="K" /> </td>
-        <td><Chart data={pressures} color="green" units="hPa" /></td>
-        <td><Chart data={humidities} color="black" units="%" /></td>
+        <td>
+          <h1>{name}</h1>
+        </td>
+        <td>
+          <Chart data={temps} color="red" units="°C Average" />{" "}
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa" />
+        </td>
+        <td>
+          <Chart data={humidities} color="black" units="%" />
+        </td>
       </tr>
     );
   }
@@ -36,18 +40,14 @@ class WeatherList extends React.Component {
             <th>Humidity(%)</th>
           </tr>
         </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
+        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
       </table>
     );
   }
 }
 
 const mapStateToProps = ({ weather }) => {
-
   return { weather };
+};
 
-}
-
-export default connect(mapStateToProps)(WeatherList)
+export default connect(mapStateToProps)(WeatherList);
