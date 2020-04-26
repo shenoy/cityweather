c1196e40bd648e5bcabbd8bd2214d78758fc1257
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-04-26 23:54:45 +0100

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '0ac87fba5d8fd8fb44b1bb53ed3bdfce';
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 
 
